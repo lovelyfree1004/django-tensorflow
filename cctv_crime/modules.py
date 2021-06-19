635d86e2035ae4394f8e517f00b6a2f163eef079
lovelyfree1004 <lovelyfree1004@gmail.com>
2021-06-19 17:00:52 +0900

    update

--- cctv_crime/modules.py
+++ cctv_crime/modules.py
@@ -5,6 +5,7 @@ import numpy as np
 import json
 import googlemaps
 from sklearn import preprocessing
+import folium
 
 @dataclass
 class File(object):
@@ -69,23 +70,20 @@ class Reader(ReaderBase):
         return json.load(open(f'{self.new_file(file)}.json', encoding='UTF-8'))
 
     def gmaps(self) -> object:
-        return googlemaps.Client(key='AIzaSyBlFNHA2GVbUuazKPvGYO9oYY2UVUSs78E')
+        return googlemaps.Client(key='AIzaSyCmfXM1pd-g-WsNCdWNJwfMOuFR828y5IE')
 
 class Printer(PrinterBase):
-
     def dframe(self, this):
         print('*' * 100)
         print(f'1. Target type \n {type(this)} ')
         print(f'2. Target column \n {this.columns} ')
         print(f'3. Target top 1개 행\n {this.head(1)} ')
         print(f'4. Target bottom 1개 행\n {this.tail(1)} ')
-        print(f'5. Target null 의 갯수\n {this.isnull().sum()}개')
+        print(f'4. Target null 의 갯수\n {this.isnull().sum()}개')
         print('*' * 100)
 
 '''
-
-crime_in_seoul
-****************************************************************************************************
+**************************************** crime in seoul ****************************************************
 1. Target type 
  <class 'pandas.core.frame.DataFrame'> 
 2. Target column 
@@ -98,7 +96,7 @@ crime_in_seoul
 4. Target bottom 1개 행
      관서명  살인 발생  살인 검거  강도 발생  강도 검거  강간 발생  강간 검거  절도 발생  절도 검거  폭력 발생  폭력 검거
 30  수서서     10      7      6      6    149    124   1439    666   1819   1559 
-5. Target null 의 갯수
+4. Target null 의 갯수
  관서명      0
 살인 발생    0
 살인 검거    0
@@ -111,10 +109,7 @@ crime_in_seoul
 폭력 발생    0
 폭력 검거    0
 dtype: int64개
-****************************************************************************************************
-
-cctv_in_seoul
-****************************************************************************************************
+********************************************* cctv in seoul ***************************************************
 1. Target type 
  <class 'pandas.core.frame.DataFrame'> 
 2. Target column 
@@ -125,18 +120,7 @@ cctv_in_seoul
 4. Target bottom 1개 행
      기관명   소계  2013년도 이전  2014년  2015년  2016년
 24  중랑구  660        509    121    177    109 
-5. Target null 의 갯수
- 기관명          0
-소계           0
-2013년도 이전    0
-2014년        0
-2015년        0
-2016년        0
-dtype: int64개
-****************************************************************************************************
-
-pop_in_seoul
-****************************************************************************************************
+*****************************************  pop_in_seoul  *****************************************
 1. Target type 
  <class 'pandas.core.frame.DataFrame'> 
 2. Target column 
@@ -147,18 +131,7 @@ pop_in_seoul
 4. Target bottom 1개 행
      자치구   계  계.1  계.2  65세이상고령자
 26  NaN NaN  NaN  NaN       NaN 
-5. Target null 의 갯수
- 자치구         1
-계           1
-계.1         1
-계.2         1
-65세이상고령자    1
-dtype: int64개
-****************************************************************************************************
-
-
 '''
-
 
 class Service(Reader):
 
@@ -186,19 +159,19 @@ class Service(Reader):
         station_lngs = []
         gmaps = reader.gmaps()
         for name in station_names:
-            temp = gmaps.geocode(name, language='KO')
+            temp = gmaps.geocode(name, language='ko')
             station_addrs.append(temp[0].get('formatted_address'))
-            t_loc = tmep[0].get('geometry')
+            t_loc = temp[0].get('geometry')
             station_lats.append(t_loc['location']['lat'])
-            station_lats.append(t_loc['location']['lng'])
-            print(f'name{temp[0].get("formatted_address")}')
+            station_lngs.append(t_loc['location']['lng'])
+            # print(f'name{temp[0].get("formatted_address")}')
         gu_names = []
         for name in station_addrs:
             t = name.split()
             gu_name = [gu for gu in t if gu[-1] == '구'][0]
             gu_names.append(gu_name)
-        crime['구분'] = gu_names
-        # 구와 경철서 위치가 다른 경우 수작업
+        crime['구별'] = gu_names
+        # 구와 경찰서 위치가 다른 경우 수작업
         crime.loc[crime['관서명'] == '혜화서', ['구별']] == '종로구'
         crime.loc[crime['관서명'] == '서부서', ['구별']] == '은평구'
         crime.loc[crime['관서명'] == '강서서', ['구별']] == '양천구'
@@ -218,7 +191,7 @@ class Service(Reader):
         file.fname = 'pop_in_seoul'
         pop = reader.xls(file, 2, 'B, D, G, J, N')
         # printer.dframe(pop)
-        cctv.rename(columns={cctv.columns[0]: '구별'}, inplace=True)
+        cctv.rename(columns={cctv.columns[0]: '구별'},inplace=True)
         pop.rename(columns={
             pop.columns[0]: '구별',
             pop.columns[1]: '인구수',
@@ -226,7 +199,7 @@ class Service(Reader):
             pop.columns[3]: '외국인',
             pop.columns[4]: '고령자'
         }, inplace=True)
-        pop.drop([26], inplace=True)
+        pop.drop([26],inplace=True)
         pop['외국인비율'] = pop['외국인'].astype(int) / pop['인구수'].astype(int) * 100
         pop['고령자비율'] = pop['고령자'].astype(int) / pop['인구수'].astype(int) * 100
         cctv.drop(['2013년도 이전', '2014년', '2015년', '2016년'], 1, inplace=True)
@@ -261,7 +234,6 @@ class Service(Reader):
         file.context = './saved_data/'
         file.fname = 'police_pos'
         police_pos = reader.csv(file)
-        # printer.dframe(police_pos)
         police = pd.pivot_table(police_pos, index='구별', aggfunc=np.sum)
         police['살인검거율'] = (police['살인 검거'].astype(int) / police['살인 발생'].astype(int)) * 100
         police['강도검거율'] = (police['강도 검거'].astype(int) / police['강도 발생'].astype(int)) * 100
@@ -279,14 +251,110 @@ class Service(Reader):
             '폭력 발생': '폭력'
         }, inplace=True)
         x = police[self.crime_rate_columns].values
-        min_max_scalar = preprocessing.minmax_scaler()
+        min_max_scalar = preprocessing.MinMaxScaler()
+        """          
+        스케일링은 선형변환을 적용하여          
+        전체 자료의 분포를 평균 0, 분산 1이 되도록 만드는 과정
+        정규화 normalization         
+        많은 양의 데이터를 처리함에 있어 데이터의 범위(도메인)를 일치시키거나         
+        분포(스케일)를 유사하게 만드는 작업
+        """
+        x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        police_norm = pd.DataFrame(x_scaled, columns=self.crime_columns, index=police.index)
+        police_norm[self.crime_rate_columns] = police[self.crime_rate_columns]
+        police_norm['범죄'] = np.sum(police_norm[self.crime_rate_columns], axis=1)
+        police_norm['검거'] = np.sum(police_norm[self.crime_columns], axis=1)
+        police_norm.to_csv('./saved_data/police_norm.csv', sep=',', encoding='UTF-8')
 
+    def folium_test(self):
+        file = self.file
+        reader = self.reader
+        printer = self.printer
+        file.context = './data/'
+        file.fname = 'us_unemployment'
+        state_data = reader.csv(file)
+        file.fname = 'us-states'
+        state_geo = reader.json(file)
 
+        bins = list(state_data["Unemployment"].quantile([0, 0.25, 0.5, 0.75, 1]))
 
+        m = folium.Map(location=[48, -102], zoom_start=3)
+
+        folium.Choropleth(
+            geo_data=state_geo,
+            data=state_data,
+            columns=["State", "Unemployment"],
+            key_on="feature.id",
+            fill_color="BuPu",
+            fill_opacity=0.7,
+            line_opacity=0.5,
+            legend_name="Unemployment Rate (%)",
+            bins=bins,
+            reset=True,
+        ).add_to(m)
+
+        m.save('./saved_data/Unemployment.html')
+
+    def draw_crime_map(self):
+        file = self.file
+        reader = self.reader
+        file.context = './saved_data/'
+        file.fname = 'police_norm'
+        police_norm = reader.csv(file)
+        file.context = './data/'
+        file.fname = 'geo_simple'
+        state_geo = reader.json(file)
+        file.fname = 'crime_in_seoul'
+        crime = reader.csv(file)
+        file.context = './saved_data/'
+        file.fname = 'police_pos'
+        police_pos = reader.csv(file)
+        station_names = []
+        for name in crime['관서명']:
+            station_names.append('서울' + str(name[:-1] + '경찰서'))
+        station_addrs = []
+        station_lats = []
+        station_lngs = []
+        gmaps = reader.gmaps()
+        for name in station_names:
+            temp = gmaps.geocode(name, language='ko')
+            station_addrs.append(temp[0].get('formatted_address'))
+            t_loc = temp[0].get('geometry')
+            station_lats.append(t_loc['location']['lat'])
+            station_lngs.append(t_loc['location']['lng'])
+
+        police_pos['lat'] = station_lats
+        police_pos['lng'] = station_lngs
+        col = ['살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거']
+        tmp = police_pos[col] / police_pos[col].max()
+        police_pos['검거'] = np.sum(tmp, axis=1)
+
+        folium_map = folium.Map(location=[37.5502, 126.982], zoom_start=12, title='Stamen Toner')
+
+        folium.Choropleth(
+            geo_data=state_geo,
+            data=tuple(zip(police_norm['구별'],police_norm['범죄'])),
+            columns=["State", "Crime Rate"],
+            key_on="feature.id",
+            fill_color="PuRd",
+            fill_opacity=0.7,
+            line_opacity=0.2,
+            legend_name="Crime Rate (%)",
+            reset=True,
+        ).add_to(folium_map)
+        for i in police_pos.index:
+            folium.CircleMarker([police_pos['lat'][i], police_pos['lng'][i]],
+                                radius=police_pos['검거'][i] * 10,
+                                fill_color='#0a0a32').add_to(folium_map)
+
+        folium_map.save('./saved_data/crime_map.html')
 
 
 if __name__ == '__main__':
 
     s = Service()
-    #s.save_police_pos()
-    s.save_cctv_pop()
+    # s.save_police_pos()
+    # s.save_cctv_pop()
+    # s.save_police_norm()
+    # s.folium_test()
+    s.draw_crime_map()
